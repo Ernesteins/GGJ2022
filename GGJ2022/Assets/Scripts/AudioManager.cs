01279255b9dbd6bd91d62df78596b51c20657578
IsmaLB <bravoismael26@gmail.com>
2022-01-30 17:12:22 -0500

    Audio scripts

---
+++ GGJ2022/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource effectsAudioSource = null;
+    [SerializeField] AudioEffects audioEffects = null;
+    static AudioManager instance = null;
+    
+    public static void Play(AudioEffect effect){
+        instance?.audioEffects.Play(effect,instance.effectsAudioSource);
+    }
+    
+    private void OnEnable(){
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        Destroy(this);
+    }
+    private void OnDisable(){
+        if(instance == this){
+            instance = null;
+        }
+    }
+}
